27ef1715bf32c5b62343117e5efd97f6c1def286
Barbany <oriolbm96@gmail.com>
2024-09-03 15:24:13 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+import glob
+import os.path as osp
+
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+this_dir = osp.dirname(osp.abspath(__file__))
+_ext_src_root = osp.join("fps_cuda", "_ext-src")
+_ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
+    osp.join(_ext_src_root, "src", "*.cu")
+)
+
+requirements = ["torch>=1.4"]
+
+setup(
+    name="fps_cuda",
+    version="3.0.0",
+    install_requires=requirements,
+    packages=find_packages(),
+    ext_modules=[
+        CUDAExtension(
+            name="fps_cuda._ext",
+            sources=_ext_sources,
+            extra_compile_args={
+                "cxx": ["-O3"],
+                "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
+            },
+            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
+    include_package_data=True,
+)
